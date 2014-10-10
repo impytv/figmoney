72bdab7cf13649529fb41234e68bfad7e82500ca
TV <trondve@gmail.com>
2014-10-10 09:54:56 +0200

    Backup script

---
+++ db/backup_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+_now=$(date +%y-%m-%d)
+_file="db_backup_$_now.sql"
+
+echo "Starting backup to $_file on $HOSTNAME"
+pg_dump -h$HOSTNAME -p49153 -Upostgres postgres > "$_file"
+
+echo "Compressing $_file before upload"
+gzip -9 -f "$_file"
+
+echo "Uploading $_file.gz to S3"
+aws s3 cp "$_file.gz" s3://impymoneybackup/
+echo "Upload done"
