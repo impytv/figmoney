a509bb0dcb705d5579e48ef301343f7e014698b4
TV <trondve@gmail.com>
2014-10-15 21:52:41 +0200

    Fix bug

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -19,21 +19,26 @@ class TransactionsController < ApplicationController
   end
 
   def create
+    @user = current_user
+
     ActiveRecord::Base.transaction do
       transaction = Transaction.new(balance_transaction_params)
+      transaction.user_id = @user.id
       transaction.actual = true
       transaction.committed = true
       transaction.date = Date.today
       transaction.description = 'Actual balance'
       transaction.save
 
-      commit = "UPDATE transactions SET committed = true WHERE date <= CURRENT_DATE"
+      commit = "UPDATE transactions SET committed = true WHERE date <= CURRENT_DATE AND user_id = #{@user.id}"
       Transaction.connection.execute(commit)
 
       last_iteration_update = "UPDATE recurring_transactions r
                                   SET last_iteration = t.max_iteration
                                  FROM (SELECT MAX(iteration) AS max_iteration, recurrence_id FROM transactions WHERE committed = true GROUP BY recurrence_id) t
-                                WHERE t.recurrence_id = r.id"
+                                WHERE t.recurrence_id = r.id
+                                  AND t.user_id = #{@user.id}
+                                  AND r.user_id = #{@user.id} "
 
       Transaction.connection.execute(last_iteration_update)
     end
