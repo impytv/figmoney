5c98e0281ca389cad9dc2a1a686413e8fa0d5647
TV <trondve@gmail.com>
2014-11-19 21:14:41 +0100

    Add monthly transactions missing files

---
+++ app/controllers/transactions_by_month_controller.rb
@@ -0,0 +1,47 @@
+class TransactionsByMonthController < ApplicationController
+
+  def index
+    @user = current_user
+    transactions = Transaction.where("user_id = #{@user.id}").order(date: :asc, actual: :asc, delta: :desc, description: :asc)
+
+    transactions = process_transactions(transactions)
+
+    @monthly_transactions = []
+    last_month = 0
+    last_amount = 0
+    previous_month_amount = 0
+
+    transactions.each do |transaction|
+      if last_month != transaction.date.month
+        last_month = transaction.date.month
+        
+        monthly_transaction = Transaction.new
+        monthly_transaction.date = Date.new(transaction.date.year, transaction.date.month, 1)
+        monthly_transaction.amount = last_amount
+        monthly_transaction.delta = last_amount - previous_month_amount
+        monthly_transaction.description = transaction.date.strftime('%Y %B')
+
+        @monthly_transactions.push(monthly_transaction)
+
+        previous_month_amount = last_amount
+      end
+
+      last_amount = transaction.amount
+    end
+  end
+
+  def process_transactions(transactions)
+    amount = 0.0
+    transactions.each do |transaction|
+      if transaction.actual
+        amount = transaction.amount
+      else
+        amount = amount + transaction.delta
+        transaction.amount = amount
+      end
+    end
+
+    return transactions
+  end
+
+end
---
+++ app/helpers/transactions_by_month_helper.rb
@@ -0,0 +1,2 @@
+module TransactionsByMonthHelper
+end
